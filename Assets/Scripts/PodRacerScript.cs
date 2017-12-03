97157b48dcca62aa3b9abdb524e8be5a00a00d39
kekos91 <kekos91@free.fr>
2017-12-03 22:54:00 +0100

    organized everything

--- Assets/Scripts/PodRacerScript.cs
+++ Assets/Scripts/PodRacerScript.cs
@@ -6,7 +6,7 @@ using UnityEngine.UI;
 
 [RequireComponent(typeof(Rigidbody))]
 public class PodRacerScript : MonoBehaviour {
-    public GameObject[] hoverPoints;
+    public GameObject hoverPoint;
     public LayerMask hoverMask; // so we don't Raycast to ourself
     public Text speedIndicator;
 
@@ -15,7 +15,7 @@ public class PodRacerScript : MonoBehaviour {
     public AnimationCurve brakeCurve;
     public AnimationCurve speedCurve;
     public float timeToFullspeed = 20.0f;
-    public float timeToFullAcceleration = 1f;
+    public float timeToFullAcceleration = 0.1f;
 
     private float accelFactor = 0f;
     private float brakeFactor = 0f;
@@ -23,29 +23,46 @@ public class PodRacerScript : MonoBehaviour {
     private float speed = 0f;
     private Dictionary<int, float> speedCurveApproximation;         // key = speed in m/s , value = factor from 0f to 1f;
     private float speedCurveApproximationPrecision = 0.0000001f;    // lower =  more accurate speeds but slower loading times
+    private Vector3 forwardVector;
     #endregion
 
-    // turn settings
-    public float maxTurnSpeed = 5f;
-    public float turnSpeedFactor = 2.0f;
+    #region Turn settings
+
+    public float maxTurnSpeed = 3.0f;
+    public float turnSpeedFactor = 3.0f;
+    private float turnOppositeMultiplier = 6.0f; // can be used for smoothing left/right transition
     private float currentTurnSpeed = 0f;
+
+    #endregion
+
+    #region Hover settings
 
     // hover settings
     private float hoverHeight = 0.5f;
-    private float hoverForce = 10f;
+    public Vector3 gravityVector = Vector3.zero;
 
-    // controls
+    #endregion
+
+    #region Controls
+
     private bool forward;
     private bool brake;
     private bool left;
     private bool right;
 
+    #endregion
+
     private Rigidbody rb;
 
     private void Start () {
+        #region Optimizations
+
         rb = GetComponent<Rigidbody>();
-        
+
+        #endregion
+
         #region Speed Approximation
+
         speedCurveApproximation = new Dictionary<int, float>();
 
         int speed = 0;
@@ -56,18 +73,21 @@ public class PodRacerScript : MonoBehaviour {
             lastFactor = factor;
             speedCurveApproximation.Add(speed, factor);
             speed += 1;
-            Debug.Log("<color=green>added "+ speed +" = "+ factor +"</color>");
+            //Debug.Log("<color=green>added "+ speed +" = "+ factor +"</color>");
         }
-        Debug.Log("speed 238 m/s (or 23.8 unity units/s) is achieved at ratio " + GetRatioForSpeed(238));
+        //Debug.Log("speed 238 m/s (or 23.8 unity units/s) is achieved at ratio " + GetRatioForSpeed(238));
+
         #endregion
     }
 
 
     private void Update () {
         forward = Input.GetKey(KeyCode.Z) || Input.GetKey(KeyCode.UpArrow);
-        brake = Input.GetKey(KeyCode.S) || Input.GetKey(KeyCode.DownArrow);
-        left = Input.GetKey(KeyCode.Q) || Input.GetKey(KeyCode.LeftArrow);
-        right = Input.GetKey(KeyCode.D) || Input.GetKey(KeyCode.RightArrow);
+        brake   = Input.GetKey(KeyCode.S) || Input.GetKey(KeyCode.DownArrow);
+        left    = Input.GetKey(KeyCode.Q) || Input.GetKey(KeyCode.LeftArrow);
+        right   = Input.GetKey(KeyCode.D) || Input.GetKey(KeyCode.RightArrow);
+
+        #region Acceleration control
 
         if (forward)
         {
@@ -81,73 +101,84 @@ public class PodRacerScript : MonoBehaviour {
         {
             accelFactor -= Time.deltaTime / timeToFullAcceleration;                                      // timeToFullAcceleration to loose acceleration. could be another number
             accelFactor = Mathf.Clamp(accelFactor, 0f, 1f);
-            speed *= 0.9994f;
+            speed *= 0.999f;
             speedFactor = GetRatioForSpeed((int)(speed*10f));                                    // get the approximated speed factor
             speedFactor = Mathf.Clamp(speedFactor, 0f, 1f);
         }
 
+        #endregion
+
+        #region Turn control
+
         if (left)
         {
-            currentTurnSpeed -= Time.deltaTime * turnSpeedFactor;
+            currentTurnSpeed -= (currentTurnSpeed > 0) ? Time.deltaTime * turnSpeedFactor * turnOppositeMultiplier : Time.deltaTime * turnSpeedFactor;
             currentTurnSpeed = Mathf.Clamp(currentTurnSpeed, -maxTurnSpeed, maxTurnSpeed);
         }
         if (right)
         {
-            currentTurnSpeed += Time.deltaTime * turnSpeedFactor;
+            currentTurnSpeed += (currentTurnSpeed < 0) ? Time.deltaTime * turnSpeedFactor * turnOppositeMultiplier : Time.deltaTime * turnSpeedFactor;
             currentTurnSpeed = Mathf.Clamp(currentTurnSpeed, -maxTurnSpeed, maxTurnSpeed);
         }
         if (!left && !right)
         {
-            currentTurnSpeed *= 0.95f;
+            currentTurnSpeed *= 1f-Time.deltaTime*2f;   // we gradually reduce turning speed;
         }
-        
-    }
 
+        #endregion
+
+    }
+    
     private void FixedUpdate()
     {
-        /*
-        foreach(GameObject hoverPoint in hoverPoints)
+        #region Hover Detection
+
+        Vector3 groundVector = Vector3.zero;
+        Debug.DrawRay(hoverPoint.transform.position, Vector3.down * hoverHeight, Color.red, 0.1f);
+        RaycastHit hit;
+        if (Physics.Raycast(hoverPoint.transform.position, Vector3.down, out hit, hoverHeight, hoverMask.value))                // if we are close enough to the ground, apply anti-gravity effect
         {
-            Debug.DrawRay(hoverPoint.transform.position, Vector3.down*hoverHeight, Color.red, 0.1f);
-            RaycastHit hit;
-            if (Physics.Raycast(hoverPoint.transform.position, Vector3.down, out hit, hoverHeight, hoverMask.value)) // if we are close enough to the ground, apply anti-gravity effect
-            {
-                Debug.Log("touché "+hit.collider.gameObject.name);
-                rb.AddForceAtPosition(hit.normal * hoverForce * rb.mass * (1.0f - (hit.distance / hoverHeight)), hoverPoint.transform.position);
-            }
-            else // if we are not close to the ground, make that part of the pod fall
-            {
-                Debug.Log("pas touché");
-                rb.AddForceAtPosition(hit.normal * -hoverForce * rb.mass * (1.0f - (hit.distance / hoverHeight)), hoverPoint.transform.position);
-            }
+            float gravityFactor = hit.distance / hoverHeight;
+            gravityVector = gravityVector * gravityFactor;
+            //gravityVector = Vector3.zero;
+            //groundVector =  hit.normal; // using hit normal so the pod slides off the surface
+            //Debug.DrawRay(hit.point, hit.normal, Color.green, 0.1f);
+            
+            //Debug.Log("<color=green>touched "+hit.collider.gameObject.name +"</color>");
         }
-        */
-        rb.velocity = transform.forward *speed;
-        transform.eulerAngles += new Vector3(0f, currentTurnSpeed, 0);
-        /*
-        if (currentBrakeFactor > 0f)
+        else // if we are not close to the ground, make that part of the pod fall
         {
-            if (rb.velocity.z > 0f)
-            {
-                rb.velocity -= new Vector3(0f, 0f, brakeCurve.Evaluate(currentBrakeFactor));
-                Debug.Log(brakeCurve.Evaluate(currentBrakeFactor));
-            }
-            Debug.Log("on freine !");
+            gravityVector += Physics.gravity * Time.fixedDeltaTime;
+            //Debug.Log("<color=red>didnt touch anything</color>");
         }
 
-        rb.AddRelativeTorque(Vector3.up * currentTurnForce);
-        */
+        #endregion
+        
+        #region Applying forward speed
+
+        forwardVector = transform.forward * speed;
+        
+        #endregion
+
+        Debug.Log(forwardVector + " , " + gravityVector + " , " + groundVector);
+        rb.velocity = forwardVector + gravityVector + groundVector;
+
+
+        #region Turning
+
+        rb.angularVelocity += new Vector3(0f, currentTurnSpeed, 0f);
+
+        #endregion
     }
-
+    
+    // Using this to update UI
     private void LateUpdate()
     {
-        speedIndicator.text = (int)(rb.velocity.magnitude * 10*3.6) + " km/h";
+        speedIndicator.text = (int)(rb.velocity.magnitude * 10f * 3.6f) + " km/h";
     }
-
-
-
-
+    
     #region Speed Approximation methods
+
     private float GetFactorForSpeed(int speed, ref float factor)
     {
         float speedFromCurve = speedCurve.Evaluate(factor) * 10;
@@ -168,5 +199,6 @@ public class PodRacerScript : MonoBehaviour {
         //Debug.Log("returning the closest lower speed ratio : " + speed);
         return speedCurveApproximation[speed];
     }
+
     #endregion
 }
