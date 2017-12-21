8a961f3d8881c06846bc7029b2cb27431ab4b6fe
kekos91 <kekos91@free.fr>
2017-12-21 15:52:09 +0100

    minimap improvements

--- Assets/Scripts/MinimapCameraScript.cs
+++ Assets/Scripts/MinimapCameraScript.cs
@@ -3,9 +3,35 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class MinimapCameraScript : MonoBehaviour {
-    public Transform playerPodTransform;
+    public Transform followedTransform;
+
+    private bool rotations = true;
+
+    //used in order to hide the shadows on the minimap
+    private ShadowQuality savedShadowQuality;
+
 	// Update is called once per frame
 	void LateUpdate () {
-        transform.position = playerPodTransform.position + new Vector3(0f, 3f, 0f);
+        transform.position = followedTransform.position + new Vector3(0f, 3f, 0f);
+        if (rotations)
+        {
+            transform.eulerAngles = new Vector3(90, followedTransform.eulerAngles.y, 0);
+        }
 	}
+
+    private void OnPreRender()
+    {
+        savedShadowQuality = QualitySettings.shadows;
+        QualitySettings.shadows = ShadowQuality.Disable;
+    }
+
+    private void OnPostRender()
+    {
+        QualitySettings.shadows = savedShadowQuality;
+    }
+
+    public void Follow(Transform t)
+    {
+        followedTransform = t;
+    }
 }
